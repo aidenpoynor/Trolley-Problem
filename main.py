4810935c1247c5cf60f578fbaeadc520b2560d55
Aiden <apoynor2005@gmail.com>
2024-10-30 12:01:25 -0600

    Debugged survey so it would run properly and added survey option to main

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import KNN
 import sys
+from survey import Survey
+
+def usage():
+    print("Here are your options:")
+    print("train    -   train KNN model")
+    print("predict  -   see what the KNN model would do in a random situation")
+    print("survey   -   decide what you would do in a random trolly problem and give it to the model")
+    print("exit     -   exit program")
+
 
 if __name__ == "__main__":
 
@@ -7,6 +16,7 @@ if __name__ == "__main__":
     data = "trolley.csv"
     while True:
         print("What would you like to do?")
+        usage()
         action = input()
 
         match action.lower():
@@ -16,16 +26,12 @@ if __name__ == "__main__":
                 sys.exit()
             case "train":
                 model = KNN.KNN(data)
-
-
             case "predict":
                 if model == None:
                     print("No current model!")
                 else: model.predict()
-
+            case "survey":
+                survey = Survey()
             case _:
-                print("Here are your options:")
-                print("train    -   train KNN model")
-                print("predict  -   see what the KNN model would do in a random situation")
-                print("survey   -   decide what you would do in a random trolly problem and give it to the model")
-                print("exit     -   exit program")
+                usage()
+
