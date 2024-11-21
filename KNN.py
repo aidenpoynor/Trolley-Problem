5dd521cead5f67493d5e111e3b0dba2a3a1b91d1
Lidd <logan.liddiard@yahoo.com>
2024-11-20 18:07:41 -0700

    Added 2D visualization for KNN AI

--- KNN.py
+++ KNN.py
@@ -1,9 +1,12 @@
-from sklearn.neighbors import KNeighborsClassifier
-import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
 class KNN:
 
-    def __init__(self,data):
+    def __init__(self, data):
         print("Building KNN")
         self.data = pd.read_csv(data)
 
@@ -24,10 +27,12 @@ class KNN:
         X = self.data.drop("decision", axis=1)  # Drop the target column
         y = self.data["decision"]  # Select the target column
 
-        self.knn.fit(X,y)
+        self.X = X
+        self.y = y
+        self.knn.fit(X, y)
     
 
-    def predict(self,predict_data):
+    def predict(self, predict_data):
         self.classes = ["num_on_main",
                         "num_on_alt",
                         "relationship_main",
@@ -38,7 +43,41 @@ class KNN:
                         "social_importance_main",
                         "social_importance_alt"]
         
-        predict = pd.DataFrame([predict_data],columns=self.classes)
-        predict = self.knn.predict(predict)
-        return predict
+        predict = pd.DataFrame([predict_data], columns=self.classes)
+        prediction = self.knn.predict(predict)
+        #TODO: fix prediction point stuff
+        #self.visualize(predict_data)
+        return prediction
     
+    def visualize(self, predict_data=None):
+        """
+        Visualize the data in 2D using PCA and optionally plot the prediction point.
+        """
+        # Perform PCA for dimensionality reduction
+        pca = PCA(n_components=2)
+        X_pca = pca.fit_transform(self.X)
+
+        # Map target labels to colors
+        colors = ['red', 'blue']
+        target_colors = [colors[label] for label in self.y]
+
+        # Plot the data points
+        plt.figure(figsize=(10, 7))
+        for i, color in enumerate(colors):
+            plt.scatter(X_pca[self.y == i, 0], X_pca[self.y == i, 1], 
+                        label=f"Class {i}", c=color, alpha=0.6)
+        
+        # Plot the prediction point if provided
+        if predict_data is not None:
+            predict_pca = pca.transform([predict_data])
+            predicted_class = self.predict(predict_data)[0]
+            plt.scatter(predict_pca[0, 0], predict_pca[0, 1], 
+                        c=colors[predicted_class], edgecolor='k', s=100, 
+                        label=f"Prediction (Class {predicted_class})", marker='X')
+
+        plt.title("KNN Data Visualization (PCA Reduced)")
+        plt.xlabel("Principal Component 1")
+        plt.ylabel("Principal Component 2")
+        plt.legend()
+        plt.savefig("AI_Visualization_KNN.png")
+        plt.cla()
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import KNN
 import sys
 import csv
+import time
 from survey import Survey
+import os
 
 def usage():
     print("Here are your options:")
     print("train    -   train KNN model")
     print("predict  -   see what the KNN model would do in a random situation")
     print("survey   -   decide what you would do in a random trolly problem and give it to the model")
-    print("exit     -   exit program")
+    print("exit     -   exit program\n\n")
+
 
 
 if __name__ == "__main__":
@@ -24,10 +27,13 @@ if __name__ == "__main__":
         print(f"Warning! You are working with {data} for you dataset!")
 
     while True:
-        print("What would you like to do?")
+        # Clearing the Screen
+        os.system('cls')
+        
+        print("What would you like to do?\n")
         usage()
         action = input()
-
+        wait = False
         match action.lower():
 
             case "exit":
@@ -36,10 +42,15 @@ if __name__ == "__main__":
             case "train":
                 print("Creating KNN model...")
                 model = KNN.KNN(data)
+                model.visualize()
+                print("KNN model created!")
+            
             case "predict":
+                wait = True
                 if model == None:
                     print("No current model!")
-                else: Survey(1,'c',model)
+                else:
+                    Survey(1,'c',model)
             case "survey":
 
                 print("\nWhat would you do in the following scenarios...\n\n\n")
@@ -58,4 +69,7 @@ if __name__ == "__main__":
 
             case _:
                 usage()
+        if wait: 
+            time.sleep(2)
+        
 
